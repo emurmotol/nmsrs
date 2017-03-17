3834f5f867fce81f704d180802714dc3a077e0df
Emur Motol <klarizonemar@gmail.com>
2017-03-17 17:45:49 +0800

    solve problems

--- main.go
+++ main.go
@@ -7,21 +7,14 @@ import (
 	"gopkg.in/unrolled/render.v1"
 )
 
-var (
-	sex              = []string{"Male", "Female"}
-	civilStatus      = []string{"Single", "Married", "Widowed", "Separated"}
-	employmentStatus = []string{"Employed", "Unemployed"}
-	unemployed       = []string{"Actively looking for work", "Resigned", "Terminated/Laid off, local", "Terminated/Laid off, abroad"}
-	disability       = []string{"Visual impairment", "Hearing impairment", "Speech impairment", "Physically handicapped"}
-)
-
 func main() {
-	r := render.New()
+	r := render.New(render.Options{
+		Directory:  "templates",
+		Extensions: []string{".gohtml", ".html"},
+	})
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		// Assumes you have a template in ./templates called "example.tmpl"
-		// $ mkdir -p templates && echo "<h1>Hello {{.}}.</h1>" > templates/example.tmpl
 		r.HTML(w, http.StatusOK, "example", "World")
 	})
 
@@ -30,135 +23,143 @@ func main() {
 	n.Run(":8080")
 }
 
-type Applicant struct {
-	PersonalInformation           PersonalInformation             `json:"personal_information"`
-	FormalEducation               []FormalEducation               `json:"formal_education"`
-	ProfessionalLicence           []ProfessionalLicence           `json:"professional_licence"`
-	Eligibility                   []Eligibility                   `json:"eligibility"`
-	TrainingAndRelevantExperience []TrainingAndRelevantExperience `json:"training_and_relevant_experience"`
-	CertificateOfCompetence       []CertificateOfCompetence       `json:"certificate_of_competence"`
-	WorkExperience                WorkExperience                  `json:"work_experience"`
-	OtherSkills                   []string                        `json:"other_skills"`
-	CreatedAt                     string                          `json:"created_at"`
-	UpdatedAt                     string                          `json:"updated_at"`
-}
+// var (
+// 	sex              = []string{"Male", "Female"}
+// 	civilStatus      = []string{"Single", "Married", "Widowed", "Separated"}
+// 	employmentStatus = []string{"Employed", "Unemployed"}
+// 	unemployed       = []string{"Actively looking for work", "Resigned", "Terminated/Laid off, local", "Terminated/Laid off, abroad"}
+// 	disability       = []string{"Visual impairment", "Hearing impairment", "Speech impairment", "Physically handicapped"}
+// )
 
-type PersonalInformation struct {
-	FamilyName            string                `json:"family_name"`
-	GivenName             string                `json:"given_name"`
-	MiddleName            string                `json:"middle_name"`
-	PresentAddress        PresentAddress        `json:"present_address"`
-	Birthdate             string                `json:"birthdate"`
-	PlaceOfBirth          string                `json:"place_of_birth"`
-	Age                   int                   `json:"age"`
-	Sex                   string                `json:"sex"`
-	Height                float32               `json:"height"`
-	Weight                float32               `json:"weight"`
-	Religion              string                `json:"religion"`
-	CivilStatus           string                `json:"civil_status"`
-	LandlineNumber        string                `json:"landline_number"`
-	MobileNumber          string                `json:"mobile_number"`
-	EmailAddress          string                `json:"email_address"`
-	EmploymentStatus      EmploymentStatus      `json:"employment_status"`
-	PreferredOccupation   []string              `json:"preferred_occupation"`
-	PreferredWorkLocation PreferredWorkLocation `json:"preferred_work_location"`
-	Disability            []string              `json:"disability"`
-	Language              Language              `json:"language"`
-}
+// type Applicant struct {
+// 	PersonalInformation           PersonalInformation             `json:"personal_information"`
+// 	FormalEducation               []FormalEducation               `json:"formal_education"`
+// 	ProfessionalLicence           []ProfessionalLicence           `json:"professional_licence"`
+// 	Eligibility                   []Eligibility                   `json:"eligibility"`
+// 	TrainingAndRelevantExperience []TrainingAndRelevantExperience `json:"training_and_relevant_experience"`
+// 	CertificateOfCompetence       []CertificateOfCompetence       `json:"certificate_of_competence"`
+// 	WorkExperience                WorkExperience                  `json:"work_experience"`
+// 	OtherSkills                   []string                        `json:"other_skills"`
+// 	CreatedAt                     string                          `json:"created_at"`
+// 	UpdatedAt                     string                          `json:"updated_at"`
+// }
 
-type PresentAddress struct {
-	HouseNumber  string `json:"house_number"`
-	Street       string `json:"street"`
-	Subdivision  string `json:"subdivision"`
-	Barangay     string `json:"barangay"`
-	Municipality string `json:"municipality"`
-	City         string `json:"city"`
-}
+// type PersonalInformation struct {
+// 	FamilyName            string                `json:"family_name"`
+// 	GivenName             string                `json:"given_name"`
+// 	MiddleName            string                `json:"middle_name"`
+// 	PresentAddress        PresentAddress        `json:"present_address"`
+// 	Birthdate             string                `json:"birthdate"`
+// 	PlaceOfBirth          string                `json:"place_of_birth"`
+// 	Age                   int                   `json:"age"`
+// 	Sex                   string                `json:"sex"`
+// 	Height                float32               `json:"height"`
+// 	Weight                float32               `json:"weight"`
+// 	Religion              string                `json:"religion"`
+// 	CivilStatus           string                `json:"civil_status"`
+// 	LandlineNumber        string                `json:"landline_number"`
+// 	MobileNumber          string                `json:"mobile_number"`
+// 	EmailAddress          string                `json:"email_address"`
+// 	EmploymentStatus      EmploymentStatus      `json:"employment_status"`
+// 	PreferredOccupation   []string              `json:"preferred_occupation"`
+// 	PreferredWorkLocation PreferredWorkLocation `json:"preferred_work_location"`
+// 	Disability            []string              `json:"disability"`
+// 	Language              Language              `json:"language"`
+// }
 
-type EmploymentStatus struct {
-	IsEmployed bool       `json:"is_employed"`
-	Unemployed string     `json:"unemployed"`
-	Terminated Terminated `json:"terminated"`
-}
+// type PresentAddress struct {
+// 	HouseNumber  string `json:"house_number"`
+// 	Street       string `json:"street"`
+// 	Subdivision  string `json:"subdivision"`
+// 	Barangay     string `json:"barangay"`
+// 	Municipality string `json:"municipality"`
+// 	City         string `json:"city"`
+// }
 
-type Terminated struct {
-	Loc     string `json:"loc"`
-	Country string `json:"country"`
-	Reason  string `json:"reason"`
-}
+// type EmploymentStatus struct {
+// 	IsEmployed bool       `json:"is_employed"`
+// 	Unemployed string     `json:"unemployed"`
+// 	Terminated Terminated `json:"terminated"`
+// }
 
-type PreferredWorkLocation struct {
-	Local          string `json:"local"`
-	Overseas       string `json:"overseas"`
-	PassportNumber string `json:"passport_number"`
-	ExpiryDate     string `json:"expiry_date"`
-}
+// type Terminated struct {
+// 	Loc     string `json:"loc"`
+// 	Country string `json:"country"`
+// 	Reason  string `json:"reason"`
+// }
 
-type Language struct {
-	Native []string `json:"native"`
-	Other  []string `json:"other"`
-}
+// type PreferredWorkLocation struct {
+// 	Local          string `json:"local"`
+// 	Overseas       string `json:"overseas"`
+// 	PassportNumber string `json:"passport_number"`
+// 	ExpiryDate     string `json:"expiry_date"`
+// }
 
-type FormalEducation struct {
-	Grade         float32 `json:"grade"`
-	Course        string  `json:"course"`
-	School        string  `json:"school"`
-	YearGraduated int     `json:"year_graduated"`
-	LastAttended  int     `jaon:"last_attended"`
-}
+// type Language struct {
+// 	Native []string `json:"native"`
+// 	Other  []string `json:"other"`
+// }
 
-type ProfessionalLicence struct {
-	Title      string `json:"title"`
-	ExpiryDate string `json:"expiry_date"`
-}
+// type FormalEducation struct {
+// 	Grade         float32 `json:"grade"`
+// 	Course        string  `json:"course"`
+// 	School        string  `json:"school"`
+// 	YearGraduated int     `json:"year_graduated"`
+// 	LastAttended  int     `jaon:"last_attended"`
+// }
 
-type Eligibility struct {
-	Title     string `json:"title"`
-	YearTaken string `json:"year_taken"`
-}
+// type ProfessionalLicence struct {
+// 	Title      string `json:"title"`
+// 	ExpiryDate string `json:"expiry_date"`
+// }
 
-type TrainingAndRelevantExperience struct {
-	Name                string `json:"name"`
-	SkillsAcquired      string `json:"skills_acquired"`
-	YearsOfExperience   int    `json:"years_of_experience"`
-	CertificateReceived string `json:"certificate_received"`
-	IssuedBy            string `json:"issued_by"`
-}
+// type Eligibility struct {
+// 	Title     string `json:"title"`
+// 	YearTaken string `json:"year_taken"`
+// }
 
-type CertificateOfCompetence struct {
-	Certificate string  `json:"certificate"`
-	Rating      float32 `json:"rating"`
-	IssuedBy    string  `json:"issued_by"`
-	DateIssued  string  `json:"date_issued"`
-}
+// type TrainingAndRelevantExperience struct {
+// 	Name                string `json:"name"`
+// 	SkillsAcquired      string `json:"skills_acquired"`
+// 	YearsOfExperience   int    `json:"years_of_experience"`
+// 	CertificateReceived string `json:"certificate_received"`
+// 	IssuedBy            string `json:"issued_by"`
+// }
 
-type WorkExperience struct {
-	Local    []Local    `json:"local"`
-	Overseas []Overseas `json:"overseas"`
-}
+// type CertificateOfCompetence struct {
+// 	Certificate string  `json:"certificate"`
+// 	Rating      float32 `json:"rating"`
+// 	IssuedBy    string  `json:"issued_by"`
+// 	DateIssued  string  `json:"date_issued"`
+// }
 
-type Local struct {
-	CompanyName                string `json:"company_name"`
-	Address                    string `json:"address"`
-	From                       string `json:"from"`
-	To                         string `json:"to"`
-	PositionHead               string `json:"position_head"`
-	IsRelatedToFormalEducation bool   `json:"is_related_to_formal_education"`
-}
+// type WorkExperience struct {
+// 	Local    []Local    `json:"local"`
+// 	Overseas []Overseas `json:"overseas"`
+// }
 
-type Overseas struct {
-	Position    string `json:"position"`
-	CompanyName string `json:"company_name"`
-	Type        string `json:"type"`
-	From        string `json:"from"`
-	To          string `json:"to"`
-}
+// type Local struct {
+// 	CompanyName                string `json:"company_name"`
+// 	Address                    string `json:"address"`
+// 	From                       string `json:"from"`
+// 	To                         string `json:"to"`
+// 	PositionHead               string `json:"position_head"`
+// 	IsRelatedToFormalEducation bool   `json:"is_related_to_formal_education"`
+// }
 
-type User struct {
-	Name      string `json:"name"`
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	Role      string `json:"role"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-}
+// type Overseas struct {
+// 	Position    string `json:"position"`
+// 	CompanyName string `json:"company_name"`
+// 	Type        string `json:"type"`
+// 	From        string `json:"from"`
+// 	To          string `json:"to"`
+// }
+
+// type User struct {
+// 	Name      string `json:"name"`
+// 	Username  string `json:"username"`
+// 	Password  string `json:"password"`
+// 	Role      string `json:"role"`
+// 	CreatedAt string `json:"created_at"`
+// 	UpdatedAt string `json:"updated_at"`
+// }
--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,3 @@
 $(function () {
-    console.log("Testing");
+    console.log("Testing lang...")
 });
