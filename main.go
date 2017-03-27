f2b64577fdf7b69a804002012f2bd8021a42affe
Emur Motol <klarizonemar@gmail.com>
2017-03-27 22:34:40 +0800

    added todos

--- main.go
+++ main.go
@@ -16,37 +16,49 @@ func layout(l string) *render.Render {
 	})
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func showHomePage(w http.ResponseWriter, r *http.Request) {
 	layout("main").HTML(w, http.StatusOK, "home/index", map[string]interface{}{
 		"title": "Home",
 	})
 }
 
-func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+func showWelcomePage(w http.ResponseWriter, r *http.Request) {
 	layout("main").HTML(w, http.StatusOK, "home/welcome", map[string]interface{}{
 		"title": "Welcome",
 	})
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request) {
+func showLoginForm(w http.ResponseWriter, r *http.Request) {
 	layout("auth").HTML(w, http.StatusOK, "auth/login", map[string]interface{}{
 		"title": "Login",
 	})
 }
 
-func registerHandler(w http.ResponseWriter, r *http.Request) {
+func login(w http.ResponseWriter, r *http.Request) {
+
+} //TODO
+
+func showRegisterForm(w http.ResponseWriter, r *http.Request) {
 	layout("auth").HTML(w, http.StatusOK, "auth/register", map[string]interface{}{
 		"title": "Register",
 	})
 }
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
+func register(w http.ResponseWriter, r *http.Request) {
+
+} //TODO
+
+func showSearchForm(w http.ResponseWriter, r *http.Request) {
 	layout("search").HTML(w, http.StatusOK, "search/index", map[string]interface{}{
 		"title": "Search",
 	})
 }
 
-func resultsHandler(w http.ResponseWriter, r *http.Request) {
+func search(w http.ResponseWriter, r *http.Request) {
+
+} //TODO
+
+func showResultsPage(w http.ResponseWriter, r *http.Request) {
 	layout("search").HTML(w, http.StatusOK, "search/results", map[string]interface{}{
 		"title": "Results",
 	})
@@ -55,12 +67,19 @@ func resultsHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", homeHandler).Methods("GET")
-	r.HandleFunc("/welcome", welcomeHandler).Methods("GET")
-	r.HandleFunc("/login", loginHandler).Methods("GET")
-	r.HandleFunc("/register", registerHandler).Methods("GET")
-	r.HandleFunc("/search", searchHandler).Methods("GET")
-	r.HandleFunc("/results", resultsHandler).Methods("GET")
+	r.HandleFunc("/", showHomePage).Methods("GET")
+	r.HandleFunc("/welcome", showWelcomePage).Methods("GET")
+
+	r.HandleFunc("/login", showLoginForm).Methods("GET")
+	r.HandleFunc("/login", login).Methods("POST") //TODO
+
+	r.HandleFunc("/register", showRegisterForm).Methods("GET")
+	r.HandleFunc("/register", register).Methods("POST") //TODO
+
+	r.HandleFunc("/search", showSearchForm).Methods("GET")
+	r.HandleFunc("/search", search).Methods("POST") //TODO
+
+	r.HandleFunc("/results", showResultsPage).Methods("GET")
 
 	h := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
 	r.PathPrefix("/static/").Handler(h).Methods("GET")
