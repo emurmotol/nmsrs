358ac49f1993059430240c592c1811b6cb544dca
Emur Motol <klarizonemar@gmail.com>
2017-03-28 18:25:26 +0800

    organize dir structure

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	ctrl "github.com/zneyrl/nmsrs-lookup/app/controllers"
+	"github.com/zneyrl/nmsrs-lookup/controllers"
 )
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Static("./static"))
-	e.GET("/", ctrl.ShowHomePage)
-	e.GET("/login", ctrl.ShowLoginForm)
+	e.GET("/", controllers.ShowHomePage)
+	e.GET("/login", controllers.ShowLoginForm)
 	e.Logger.Fatal(e.Start(":1323"))
 }
